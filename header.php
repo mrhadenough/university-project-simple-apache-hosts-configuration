cce9314de8c88a6160d50b88752589d124fd058f
Konstantin Shulyak <f260130@drdrb.com>
2014-05-21 17:27:46 +0300

    add title

--- header.php
+++ header.php
@@ -7,10 +7,9 @@ Website: http://www.realitysoftware.ca
 Note: This is a free template released under the Creative Commons Attribution 3.0 license,
 which means you can use it in any way you want provided you keep the link to the author intact.
 -->
-<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title></title>
+    <title>СИСТЕМА КОНТРОЛЮ ВІРТУАЛЬНИХ ХОСТІВ</title>
     <link href="/css/style.css" rel="stylesheet" type="text/css" />
     <script src="/js/jquery-2.1.0.min.js"></script>
     <script src="/js/default.js"></script>
