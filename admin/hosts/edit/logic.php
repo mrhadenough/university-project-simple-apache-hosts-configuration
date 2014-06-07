d9badb822b05853f49da905e29964d3d755d037e
Konstantin Shulyak <f260130@drdrb.com>
2014-06-07 18:12:54 +0300

    success message

--- admin/hosts/edit/logic.php
+++ admin/hosts/edit/logic.php
@@ -39,5 +39,5 @@ if (isset($_REQUEST['act']) && $_REQUEST['act'] == 'addHost' && isset($_REQUEST[
     $f = fopen($_SERVER['DOCUMENT_ROOT'] . "/tmp/hosts_add/$name.conf",'w');
     fwrite($f, $_REQUEST['config']);
     fclose($f);
-    echo 'aaaaaa';
+    echo '<p class="success-message">News setting has been saved</p>';
 }
